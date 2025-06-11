913069391e0ece7bbf05cf04673c80f23cec6cf4
Win Cheng <winzamark12@gmail.com>
2025-06-10 18:57:38 -0700

    init

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="h-30 w-30 flex flex-col border border-black"></div>
-    </main>
-  );
-}
